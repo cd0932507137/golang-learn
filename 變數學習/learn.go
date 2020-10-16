04beb42a6de5d18c0f70231d9a535b1aabd4f0fe
tim_jiang <tim_jiang@mail.chungyo.net>
2020-10-16 16:06:09 +0800

    變數學習

---
+++ 變數學習/learn.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+var (
+	x      int
+	y      int
+	z      int
+	c      bool
+	python bool
+	java   bool
+)
+
+// 初始化變數
+var (
+	q    int  = 1
+	a    bool = true
+	text      = "test text"
+)
+
+func main() {
+	demo := "demo 123"
+	fmt.Println(x, y, z, c, python, java)
+	fmt.Println(q, a, text, demo)
+}
